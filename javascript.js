bcd28f798e850748e4858bacda741d90ecd6e175
itsvinoth <148236871+itsvinoth@users.noreply.github.com>
2023-11-27 14:25:59 +0530

    Update javascript.js new

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const scrollRevealConfig = {
     reset: true,
     delay: 300
 };
+
 ScrollReveal().reveal('.bot1', scrollRevealConfig);
 ScrollReveal().reveal('.hello', scrollRevealConfig);
 ScrollReveal().reveal('.im', scrollRevealConfig);
@@ -72,19 +73,84 @@ function typeEffect() {
 typeEffect();
 
 // Carousel
-let slideIndex = 0;
-showSlides();
+let onSlide = false;
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); 
+window.addEventListener("load", () => {
+    autoSlide();
+
+    const dots = document.querySelectorAll(".carousel_dot");
+    dots.forEach((dot, index) => dot.addEventListener("click", () => slide(index)));
+
+    const buttonPrev = document.querySelector(".carousel_button__prev");
+    const buttonNext = document.querySelector(".carousel_button__next");
+    buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
+    buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
+});
+
+function autoSlide() {
+    setInterval(() => {
+        slide(getItemActiveIndex() + 1);
+    }, 3000);
+}
+
+function slide(toIndex) {
+    if (onSlide) return;
+    onSlide = true;
+
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+    const itemActiveIndex = itemsArray.indexOf(itemActive);
+    let newItemActive = null;
+
+    if (toIndex > itemActiveIndex) {
+        if (toIndex >= itemsArray.length) {
+            toIndex = 0;
+        }
+
+        newItemActive = itemsArray[toIndex];
+        newItemActive.classList.add("carousel_item__pos_next");
+        setTimeout(() => {
+            newItemActive.classList.add("carousel_item__next");
+            itemActive.classList.add("carousel_item__next");
+        }, 20);
+    } else {
+        if (toIndex < 0) {
+            toIndex = itemsArray.length - 1;
+        }
+
+        newItemActive = itemsArray[toIndex];
+        newItemActive.classList.add("carousel_item__pos_prev");
+        setTimeout(() => {
+            newItemActive.classList.add("carousel_item__prev");
+            itemActive.classList.add("carousel_item__prev");
+        }, 20);
+    }
+
+    newItemActive.addEventListener("transitionend", () => {
+        itemActive.className = "carousel_item";
+        newItemActive.className = "carousel_item carousel_item__active";
+        onSlide = false;
+    }, {
+        once: true
+    });
+
+    slideIndicator(toIndex);
+}
+
+function getItemActiveIndex() {
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+    const itemActiveIndex = itemsArray.indexOf(itemActive);
+    return itemActiveIndex;
+}
+
+function slideIndicator(toIndex) {
+    const dots = document.querySelectorAll(".carousel_dot");
+    const dotActive = document.querySelector(".carousel_dot__active");
+    const newDotActive = dots[toIndex];
+
+    dotActive.classList.remove("carousel_dot__active");
+    newDotActive.classList.add("carousel_dot__active");
 }
 
 // Input Validation
